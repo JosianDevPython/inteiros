4b248186c6144a88f22493d9792743b257e0fa6b
JosianDevPython <josianhenrique.devpython@gmail.com>
2024-11-01 08:46:48 -0300

    numeros inteiros

---
+++ inteiros/numero_oito.py
@@ -0,0 +1,11 @@
+soma = 3 + 5
+print(soma)
+
+subtracao = 10 - 2
+print(subtracao)
+
+multiplicacao = 2 * 4
+print(multiplicacao)
+
+divisao = 16 / 2
+print(divisao)
---
+++ numero_favorito.py
@@ -0,0 +1,3 @@
+favorito = 7
+mensagem = f'Meu número favorito é {favorito}'
+print(mensagem)
